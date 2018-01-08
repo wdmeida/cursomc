09415855a579247a8c7267714f51a63d297b296f
Wagner Almeida <wdmeida@gmail.com>
2018-01-08 14:22:29 -0200

    Creating repository and service to Categoria

---
+++ src/main/java/com/wagneralmeida/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.wagneralmeida.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wagneralmeida.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {}
