e523623e8dac6b807e72d2c4c87e109d0afa7cb4
Wagner Almeida <wdmeida@gmail.com>
2018-01-09 16:13:54 -0200

    Fixing to endpoint /categorias/{id}

--- src/main/java/com/wagneralmeida/cursomc/services/CategoriaService.java
+++ src/main/java/com/wagneralmeida/cursomc/services/CategoriaService.java
@@ -5,15 +5,21 @@ import org.springframework.stereotype.Service;
 
 import com.wagneralmeida.cursomc.domain.Categoria;
 import com.wagneralmeida.cursomc.repositories.CategoriaRepository;
+import com.wagneralmeida.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
 	
 	public Categoria buscar(Integer id) {
-		Categoria obj = repo.findOne(id);
+		Categoria obj = categoriaRepository.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}	
 		return obj;
 	}
 }
