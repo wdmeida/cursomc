8e20222daf4e3aa360fe22884d054fc2ff04c6b3
Wagner Almeida <wdmeida@gmail.com>
2018-01-10 17:00:20 -0200

    Created endpoint /clientes/{id}

---
+++ src/main/java/com/wagneralmeida/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.wagneralmeida.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wagneralmeida.cursomc.domain.Cliente;
+import com.wagneralmeida.cursomc.repositories.ClienteRepository;
+import com.wagneralmeida.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = clienteRepository.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}	
+		return obj;
+	}
+}
