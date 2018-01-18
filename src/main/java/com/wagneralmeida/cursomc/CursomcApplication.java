b569668dcac85684597fd0421034a33269d6e601
Wagner Almeida <wdmeida@gmail.com>
2018-01-18 08:52:13 -0200

    Adding Entities Pedido, Estado and Pagamento

--- src/main/java/com/wagneralmeida/cursomc/CursomcApplication.java
+++ src/main/java/com/wagneralmeida/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.wagneralmeida.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.wagneralmeida.cursomc.domain.Cidade;
 import com.wagneralmeida.cursomc.domain.Cliente;
 import com.wagneralmeida.cursomc.domain.Endereco;
 import com.wagneralmeida.cursomc.domain.Estado;
+import com.wagneralmeida.cursomc.domain.Pagamento;
+import com.wagneralmeida.cursomc.domain.PagamentoComBoleto;
+import com.wagneralmeida.cursomc.domain.PagamentoComCartao;
+import com.wagneralmeida.cursomc.domain.Pedido;
 import com.wagneralmeida.cursomc.domain.Produto;
+import com.wagneralmeida.cursomc.domain.enums.EstadoPagamento;
 import com.wagneralmeida.cursomc.domain.enums.TipoCliente;
 import com.wagneralmeida.cursomc.repositories.CategoriaRepository;
 import com.wagneralmeida.cursomc.repositories.CidadeRepository;
 import com.wagneralmeida.cursomc.repositories.ClienteRepository;
 import com.wagneralmeida.cursomc.repositories.EnderecoRepository;
 import com.wagneralmeida.cursomc.repositories.EstadoRepository;
+import com.wagneralmeida.cursomc.repositories.PagamentoRepository;
+import com.wagneralmeida.cursomc.repositories.PedidoRepository;
 import com.wagneralmeida.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -83,7 +95,22 @@ public class CursomcApplication implements CommandLineRunner {
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
 		clienteRepository.save(Arrays.asList(cli1));
-		
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1),
+			   ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6),
+				  pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		
+		ped1.setPagamento(pagto1);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
 	}
 }
---
+++ src/main/java/com/wagneralmeida/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.wagneralmeida.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wagneralmeida.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {}
