3ef2f3426621a4eadaeb9a42c8224bf48dc4149d
Wagner Almeida <wdmeida@gmail.com>
2018-01-26 18:28:54 -0200

    Adding syntax validation with validation beans

--- src/main/java/com/wagneralmeida/cursomc/services/CategoriaService.java
+++ src/main/java/com/wagneralmeida/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wagneralmeida.cursomc.domain.Categoria;
+import com.wagneralmeida.cursomc.dto.CategoriaDTO;
 import com.wagneralmeida.cursomc.repositories.CategoriaRepository;
 import com.wagneralmeida.cursomc.services.exceptions.DataIntegrityException;
 import com.wagneralmeida.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
