f43951ba58ba8abee834d4ed625e480888b3d9cd
Wagner Almeida <wdmeida@gmail.com>
2018-02-28 17:28:29 -0300

    Add custom validation cpf and cnpj

--- src/main/java/com/wagneralmeida/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/wagneralmeida/cursomc/dto/ClienteNewDTO.java
@@ -2,28 +2,45 @@ package com.wagneralmeida.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.wagneralmeida.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	// Cliente
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	// Endereços
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
 	
-	// Telefones (um será obrigatório)
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
 	
-	// Cidade
 	private Integer cidadeId;
 
 	public ClienteNewDTO() { }
